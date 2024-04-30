4f926fe3e8727ffae7e97fd6372b9994592a45c6
zanutt <matszg@live.com>
2024-04-29 21:17:07 -0300

    adcionado funcionalidades

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import TodoListImg from './assets/logo.png'
 
 import { 
@@ -8,6 +9,19 @@ import {
  } from './styles'
 
 export default function App() {
+  const [tasks, setTasks] = useState([{ id: Math.random(), text: "Tarefa 1" },
+                                      { id: Math.random(), text: "Tarefa Nova" }])
+
+   function insertNewTask(event){
+    if (event.key === "Enter"){
+      setTasks([...tasks, { id: Math.random(), text: event.target.value }])
+      event.target.value = ""
+    }
+   }
+   function removeTask(id) {
+    setTasks(tasks.filter((task)=> task.id !== id))
+   }
+
   return (<>
     <MainContainer>
       <Container>
@@ -21,15 +35,21 @@ export default function App() {
         <RightContainer>
           <Titulo>Minhas Tarefas</Titulo>
           <SubTitulo>Tarefas do dia...</SubTitulo>
+          {tasks.map((task) => (
+            <Tarefa key={task.id} onClick={() => removeTask(task.id)}>
+              <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="35" height="35" rx="11.25" fill="#35383E" fill-opacity="0.1" />
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M17.5 19.3183L22.9548 24.7731L24.7731 22.9548L19.3183 17.5L24.7731 12.0452L22.9548 10.2269L17.5 15.6817L12.0452 10.2269L10.2269 12.0452L15.6817 17.5L10.2269 22.9548L12.0452 24.7731L17.5 19.3183ZM17.5 19.3183L19.3183 17.5L17.5 15.6817L15.6817 17.5L17.5 19.3183Z" fill="#35383E" fill-opacity="0.25" />
+              <path d="M17.5 19.3183L19.3183 17.5L17.5 15.6817L15.6817 17.5L17.5 19.3183Z" fill="#35383E" fill-opacity="0.25" />
+            </svg>
+              <p>{task.text}</p>
+            </Tarefa>
+          )) }
 
-          <Tarefa><svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="35" height="35" rx="11.25" fill="#35383E" fill-opacity="0.1" />
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M17.5 19.3183L22.9548 24.7731L24.7731 22.9548L19.3183 17.5L24.7731 12.0452L22.9548 10.2269L17.5 15.6817L12.0452 10.2269L10.2269 12.0452L15.6817 17.5L10.2269 22.9548L12.0452 24.7731L17.5 19.3183ZM17.5 19.3183L19.3183 17.5L17.5 15.6817L15.6817 17.5L17.5 19.3183Z" fill="#35383E" fill-opacity="0.25" />
-            <path d="M17.5 19.3183L19.3183 17.5L17.5 15.6817L15.6817 17.5L17.5 19.3183Z" fill="#35383E" fill-opacity="0.25" />
-          </svg>
-          <p>Tarefa 1</p>
-          </Tarefa>
-          <TaskInfo type="text" placeholder="Digite sua próxima tarefa aqui..." />
+          
+          <TaskInfo type="text" 
+          placeholder="Digite sua próxima tarefa aqui..." 
+            onKeyDown={insertNewTask} />
         </RightContainer>
       </Container>
     </MainContainer>
