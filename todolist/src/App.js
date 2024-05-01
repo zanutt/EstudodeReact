72a1d6cda4181bab8de8fec8bfa7c8b84e5be701
zanutt <matszg@live.com>
2024-05-01 15:10:48 -0300

    Update Todo List App with Axios for API Requests
    * Add axios package for making HTTP requests
    * Fetch tasks from a remote API endpoint on component mount
    * Send a POST request to add a new task to the

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,36 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import axios from 'axios'
 import TodoListImg from './assets/logo.png'
 
-import { 
+import {
   MainContainer, Container, Titulo,
   LeftContainer, LogoImage,
   HeadLine, SubHeadLine,
   RightContainer, SubTitulo, Tarefa, TaskInfo,
- } from './styles'
+} from './styles'
 
 export default function App() {
-  const [tasks, setTasks] = useState([{ id: Math.random(), text: "Tarefa 1" },
-                                      { id: Math.random(), text: "Tarefa Nova" }])
+  const [tasks, setTasks] = useState([])
 
-   function insertNewTask(event){
-    if (event.key === "Enter"){
-      setTasks([...tasks, { id: Math.random(), text: event.target.value }])
-      event.target.value = ""
+  const taskText = useRef("");
+
+  useEffect(() => {
+    async function listTasks() {const {data: taskList} = await axios.get('http://localhost:3001/tasks/')
+    setTasks(taskList)
+  }
+  listTasks()    
+  }, [])
+
+  // http://localhost:3001/tasks
+  async function insertNewTask(event) {
+    if (event.key === "Enter") {
+      const newText = taskText.current.value;
+      const response = await axios.post('http://localhost:3001/tasks/', { text: newText });
+      const newTask = { id: response.data.id, text: newText };
+      setTasks([...tasks, newTask]);
+      taskText.current.value = "";
     }
-   }
-   function removeTask(id) {
-    setTasks(tasks.filter((task)=> task.id !== id))
-   }
+  }
+  async function removeTask(id) {
+    const dada = await axios.delete(`http://localhost:3001/tasks/${id}`)
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
 
   return (<>
     <MainContainer>
       <Container>
         <LeftContainer>
           <HeadLine>
-          <LogoImage src={TodoListImg} />
-          <Titulo>Organize sua vida com <span>simplicidade e eficiência</span></Titulo>           
+            <LogoImage src={TodoListImg} />
+            <Titulo>Organize sua vida com <span>simplicidade e eficiência</span></Titulo>
           </HeadLine>
-          <SubHeadLine>Adicione ao lado as tarefas que deseja fazer hoje e não pare até ela estar vazia!</SubHeadLine>        
+          <SubHeadLine>Adicione ao lado as tarefas que deseja fazer hoje e não pare até ela estar vazia!</SubHeadLine>
         </LeftContainer>
         <RightContainer>
           <Titulo>Minhas Tarefas</Titulo>
@@ -38,18 +52,19 @@ export default function App() {
           {tasks.map((task) => (
             <Tarefa key={task.id} onClick={() => removeTask(task.id)}>
               <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect width="35" height="35" rx="11.25" fill="#35383E" fill-opacity="0.1" />
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M17.5 19.3183L22.9548 24.7731L24.7731 22.9548L19.3183 17.5L24.7731 12.0452L22.9548 10.2269L17.5 15.6817L12.0452 10.2269L10.2269 12.0452L15.6817 17.5L10.2269 22.9548L12.0452 24.7731L17.5 19.3183ZM17.5 19.3183L19.3183 17.5L17.5 15.6817L15.6817 17.5L17.5 19.3183Z" fill="#35383E" fill-opacity="0.25" />
-              <path d="M17.5 19.3183L19.3183 17.5L17.5 15.6817L15.6817 17.5L17.5 19.3183Z" fill="#35383E" fill-opacity="0.25" />
-            </svg>
+                <rect width="35" height="35" rx="11.25" fill="#35383E" fill-opacity="0.1" />
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M17.5 19.3183L22.9548 24.7731L24.7731 22.9548L19.3183 17.5L24.7731 12.0452L22.9548 10.2269L17.5 15.6817L12.0452 10.2269L10.2269 12.0452L15.6817 17.5L10.2269 22.9548L12.0452 24.7731L17.5 19.3183ZM17.5 19.3183L19.3183 17.5L17.5 15.6817L15.6817 17.5L17.5 19.3183Z" fill="#35383E" fill-opacity="0.25" />
+                <path d="M17.5 19.3183L19.3183 17.5L17.5 15.6817L15.6817 17.5L17.5 19.3183Z" fill="#35383E" fill-opacity="0.25" />
+              </svg>
               <p>{task.text}</p>
             </Tarefa>
-          )) }
+          ))}
 
-          
-          <TaskInfo type="text" 
-          placeholder="Digite sua próxima tarefa aqui..." 
-            onKeyDown={insertNewTask} />
+
+          <TaskInfo type="text"
+            placeholder="Digite sua próxima tarefa aqui..."
+            onKeyDown={insertNewTask}
+            ref={taskText} />
         </RightContainer>
       </Container>
     </MainContainer>
